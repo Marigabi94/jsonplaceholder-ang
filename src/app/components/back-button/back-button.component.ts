d6f36c24c55300fefa9dca3fb4dbfdbc8cd8242b
Marigabi94 <marigabi940@gmail.com>
2021-12-03 19:58:18 -0400

    Visibilidad del back button y funcionabilidad.

--- src/app/components/back-button/back-button.component.ts
+++ src/app/components/back-button/back-button.component.ts
@@ -1,16 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-back-button',
   templateUrl: './back-button.component.html',
   styleUrls: ['./back-button.component.scss']
 })
-export class BackButtonComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class BackButtonComponent  {
 
   Back(){
     history.back();
